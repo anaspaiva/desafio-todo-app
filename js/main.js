c4b821472b04aa1b149977d635eb4206ad81e761
Ana Letícia Paiva <analeticia.s.paiva@gmail.com>
2022-03-30 08:10:38 -0300

    commit - third
    Início do desenvolvimento do script.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const inserirTarefa= document.querySelector('#inserir-tarefa');
+const adicionaTarefa = document.querySelector('#btn-add-tarefa');
+const listaTarefas = document.querySelector('#lista.tarefas');
+const detalhsesTarefas = document.querySelector('.detalhes-tarefas, .itens-restantes');
+const filtro = document.querySelector('.filtro button');
+const limparTudo = document.querySelector('.botao-limpar');
+
+
+
